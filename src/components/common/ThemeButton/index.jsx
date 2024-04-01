8a9232bcc4cd3e30c028b9ce8384a39298ec514d
parry_11 <pradeep.wadhwa14@gmail.com>
2024-04-01 16:22:19 +0530

    added motion animations

--- src/components/common/ThemeButton/index.jsx
+++ src/components/common/ThemeButton/index.jsx
@@ -1,13 +1,16 @@
 import { Button } from "@mui/material";
+import { motion } from "framer-motion";
 
 const ThemeButton = (props) => {
   return (
-    <Button
-      className={`btn ${
-        props.variant === "outlined" ? "btn_outlined" : "btn_contained"
-      }`}
-      {...props}
-    />
+    <motion.span whileHover={{ scale: 1.05 }}>
+      <Button
+        className={`btn ${
+          props.variant === "outlined" ? "btn_outlined" : "btn_contained"
+        }`}
+        {...props}
+      />
+    </motion.span>
   );
 };
 
