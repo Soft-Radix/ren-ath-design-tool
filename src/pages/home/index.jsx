4b2a442bc7b5d4fe3e834bafe87b7e11519902ea
parry_11 <pradeep.wadhwa14@gmail.com>
2024-04-02 11:42:00 +0530

    category section done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,8 +3,11 @@ import styles from "./home.module.scss";
 import bgImage from "../../assets/images/home/bgImage.png";
 import text from "../../assets/images/home/text.png";
 import model from "../../assets/images/home/model.png";
+import category1 from "../../assets/images/home/category1.png";
+import category2 from "../../assets/images/home/category2.png";
 import { motion } from "framer-motion";
 import SectionHeading from "../../components/common/sectionHeading";
+import { Container } from "@mui/material";
 
 const Home = () => {
   return (
@@ -31,10 +34,20 @@ const Home = () => {
           <img src={model} className={styles.modelImg} alt="" />
         </motion.div>
       </div>
-      <SectionHeading
-        heading="Our Categories"
-        subHeading="Here are some of the categories we provides"
-      />
+      <Container maxWidth="xl">
+        <SectionHeading
+          heading="Our Categories"
+          subHeading="Here are some of the categories we provides"
+        />
+        <div className={styles.categoryWrap}>
+          <img src={category1} alt="" />
+          <img src={category2} alt="" />
+        </div>
+        <SectionHeading
+          heading="Women / Girls Products"
+          subHeading="8 type of products available for women & girl"
+        />
+      </Container>
     </>
   );
 };
