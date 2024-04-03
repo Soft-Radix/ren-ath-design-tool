2a5a88f24d26763e996cf35f96be7f98c4bf88d3
parry_11 <pradeep.wadhwa14@gmail.com>
2024-04-03 17:40:31 +0530

    sidebar design done

--- src/pages/productView/sidebar/index.jsx
+++ src/pages/productView/sidebar/index.jsx
@@ -1,17 +1,35 @@
 import mainLogo from "../../../assets/images/mainLogo.png";
+import { useProductStore } from "../../../store";
 import styles from "./sidebar.module.scss";
 import sidebarItems from "./sidebarItems";
+import { motion } from "framer-motion";
 
 const Sidebar = () => {
-  const renderItem = ({ name, Icon }, index) => {
+  const selectedSidebarItem = useProductStore(
+    (state) => state.selectedSidebarItem
+  );
+  const updateSelectedSidebarItem = useProductStore(
+    (state) => state.updateSelectedSidebarItem
+  );
+
+  const renderItem = ({ id, name, Icon }) => {
     return (
-      <div className={styles.item} key={index}>
-        <div className={`${styles.line} ${styles.hideLine}`} />
+      <motion.div
+        className={styles.item}
+        key={id}
+        onClick={() => updateSelectedSidebarItem(id)}
+        whileHover={{ scale: 1.04, backgroundColor: "#768f99" }}
+      >
+        <div
+          className={`${styles.line} ${
+            selectedSidebarItem !== id ? styles.hideLine : ""
+          }`}
+        />
         <div className={styles.iconWrap}>
           <Icon />
         </div>
         <span className={styles.name}>{name}</span>
-      </div>
+      </motion.div>
     );
   };
 
--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,15 @@
 import { create } from "zustand";
 
+// For updating product in product view
 export const useProductStore = create((set) => ({
   id: "W1",
+  selectedSidebarItem: 1,
   updateProduct: (updatedId) =>
     set(() => ({
       id: updatedId,
+    })),
+  updateSelectedSidebarItem: (updatedId) =>
+    set(() => ({
+      selectedSidebarItem: updatedId,
     })),
 }));
