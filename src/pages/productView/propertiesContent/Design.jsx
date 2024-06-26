f374ef2a0b7bace9245a6c3605f59f6e435f931b
rohit <rohit.kumar@softradix.in>
2024-06-26 21:20:08 +0530

    fix design realted issue

--- src/pages/productView/propertiesContent/Design.jsx
+++ src/pages/productView/propertiesContent/Design.jsx
@@ -14,7 +14,7 @@ const Design = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 5000);
+    }, 2000);
     return () => clearTimeout(timer);
   }, []);
 
